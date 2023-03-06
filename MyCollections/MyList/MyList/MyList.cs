9e2c917ad6d0016a38bcda1e2ef89b44f702fb31
Maksim Novokhatskii <kongo1232231@mail.ru>
2023-03-06 15:33:40 +0300

    Created a new List collection and added the most basic methods to it.

---
+++ MyCollections/MyList/MyList/MyList.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyList
+{
+    class MyList<Type>
+    {
+        private Type[] _data;
+
+        private uint _size;
+        private uint _capacity;
+
+        public uint Size { private set { _size = value; } get => _size; }
+        public uint Capacity
+        {
+            get => _capacity;
+
+            set
+            {
+                if (value < _size) 
+                {
+                    Console.WriteLine("#### Err: The capacity cannot be smaller than the size! ####");
+                    return;
+                }
+
+                if (value != _size)
+                {
+                    if (value > 0)
+                    {
+                        _capacity = value;
+                        Type[] new_data = new Type[_capacity];
+
+                        if (_size > 0)
+                        {
+                            for (uint i = 0; i < _size; i++)
+                            {
+                                new_data[i] = _data[i];
+                            }
+                        }
+
+                        _data = new_data;
+                    }
+                }
+            }
+        }
+
+        public MyList() 
+        {
+            _capacity = 1;
+            _size = 0;
+
+            _data = new Type[_capacity];
+        }
+
+        public MyList(uint capacity) 
+        {
+            _capacity = capacity;
+            _size = 0;
+
+            _data = new Type[_capacity];
+        }
+
+        public MyList(uint capacity, Type item)
+        {
+            Capacity = capacity;
+            _size = _capacity;
+
+            _data = new Type[_capacity];
+            for (int i = 0; i < _size; i++) 
+            {
+                _data[i] = item;
+            }
+        }
+
+        //добавление нового элемента в список;
+        public void Add(Type item)
+        {
+            if (_size < _capacity)
+            {
+                _data[_size++] = item;
+                return;
+            }
+            else
+            {
+                AddWithResize(item);
+            }
+            
+        }
+
+        public void AddWithResize(Type item)
+        {
+            Capacity *= 2;
+            _data[_size++] = item;
+        }
+
+        public void Print() 
+        {
+            for (uint i = 0; i < _size; i++)
+            {
+                Console.WriteLine($"[{i}]: {_data[i]}");
+            }
+        }
+        
+        //копирует список в массив array;
+        public void CopyTo(Type[] array) { /*TODO*/ }
+
+        //удаляет из списка все элементы;
+        public void Clear() { /*TODO*/ }
+
+        //изменяет порядок элементов;
+        public void Reverse() { /*TODO*/ }
+
+        //вставляет элемент item в список по индексу index.
+        //Если такого индекса в списке нет, то генерируется исключение;
+        public void Insert(int index, Type item) { /*TODO*/ }
+
+        //удаление элемента по указанному индексу index.
+        //Если такого индекса в списке нет, то генерируется исключение;
+        public void RemoveAt(int index) { /*TODO*/ }
+
+        //параметр index задает индекс, с которого надо удалить элементы,
+        //а параметр count задает количество удаляемых элементов;
+        public void RemoveRange(int index, int count) { /*TODO*/ }
+
+        //возвращает список элементов,
+        //количество которых равно count, начиная с индекса index;
+        //public MyList<T> GetRange(int index, int count) { /*TODO*/ }
+    }
+}
---
+++ MyCollections/MyList/MyList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MyList;
+
+class Program
+{
+    static void Main()
+    {
+        MyList<int> list = new MyList<int>(1, 0);
+        List<int> list2 = new List<int>();
+
+        Console.WriteLine($"capacity: {list.Capacity}");
+        Console.WriteLine($"size: {list.Size}\n");
+
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+
+        Console.WriteLine("Array: ");
+        list.Print();
+
+        Console.WriteLine($"\ncapacity: {list.Capacity}");
+        Console.WriteLine($"size: {list.Size}\n\n\n");
+
+        Console.WriteLine($"capacity: {list2.Capacity}");
+        Console.WriteLine($"size: {list2.Count}\n");
+
+        list2.Add(1);
+        list2.Add(2);
+        list2.Add(3);
+        list2.Add(4);
+        list2.Add(5);
+
+        Console.WriteLine("Array: ");
+        for (int i = 0; i < list2.Count; i++)
+        {
+            Console.WriteLine(list2[i]);
+        }
+
+        Console.WriteLine($"\ncapacity: {list2.Capacity}");
+        Console.WriteLine($"size: {list2.Count}");
+    }
+}
